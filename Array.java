a0b39df35a635d3f6d8c7b4c550d24e45d0989af
MDIQBAL741 <mdiqbal09836@gmail.com>
2026-01-11 12:12:48 +0530

    variable

--- Array.java
+++ Array.java
@@ -7,7 +7,7 @@ public class Array {
 	int marks[]=new int[3];
 	void display() {
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter marks of 3 subjects ");
+		System.out.println("Enter marks of 3 subject ");
 		for(int i=0;i<marks.length;i++) {
 			marks[i]=sc.nextInt();
 		}
